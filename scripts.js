363c1a91cc7e98a56a6f61ac0e22ec79402fc619
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-04-17 20:01:05 -0300

    Debounce com set e clearTimeout

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+let time = null;
+function handleKeyUp(event) {
+
+  clearTimeout(time);
+
+  time = setTimeout(() => {
+    console.log(event.target.value);
+  }, 1000)
+}
+
+document.querySelector('input').addEventListener('keyup', handleKeyUp);
